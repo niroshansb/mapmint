1dc05d12d2322804c0a5c592eb878938d4a03f74
niroshansb <nsanj88@gmail.com>
2017-08-29 22:33:22 +0900

    Get procedure and service info

--- mapmint-services/datastores/istSOS/service.py
+++ mapmint-services/datastores/istSOS/service.py
@@ -5,6 +5,7 @@ import libxml2
 import osgeo.ogr
 import libxslt
 import zoo
+import requests
 
 def down(conf,inputs,outputs):
 	try: 
@@ -13,6 +14,22 @@ def down(conf,inputs,outputs):
 		return 4
 	try:
 		f.write(inputs["url"]["value"]+"\n");
+	except:
+		return 4
+	outputs["Result"]["value"]=zoo._("Data Store ")+inputs["name"]["value"]+zoo._(" created")
+	return zoo.SERVICE_SUCCEEDED
+
+def save(conf,inputs,outputs):
+	try:
+		r = requests.get(inputs["url"]["value"]+"/wa/istsos/services?query=&page=1&start=0&limit=25")
+	except:
+		return 4	
+	try: 
+		f = open(conf["main"]["dataPath"]+"/"+"istsos"+"/"+inputs["name"]["value"]+".txt", 'w')
+	except:
+		return 4
+	try:
+		f.write(r.content);
 	except:
 		return 4
 	outputs["Result"]["value"]=zoo._("Data Store ")+inputs["name"]["value"]+zoo._(" created")
